04f14b487ad9511005eef8ef526aec076e14ad50
Adrian Birta <52926500+AdrianBirta@users.noreply.github.com>
2025-05-06 14:47:38 +0300

    fix: cursor shadow animation fix

--- src/lib/cursor.ts
+++ src/lib/cursor.ts
@@ -1,11 +1,11 @@
 // src/lib/cursor.ts
 
 export function enableCursorStyle() {
-  const cursorShadow = document.querySelector(".cursor-style");
+  const cursorStyle = document.querySelector(".cursor-style") as HTMLElement | null;
   const links = document.querySelectorAll(".link");
 
-  const handleLinkHover = () => cursorShadow?.classList.add("scale-up");
-  const handleLinkLeave = () => cursorShadow?.classList.remove("scale-up");
+  const handleLinkHover = () => cursorStyle?.classList.add("scale-up");
+  const handleLinkLeave = () => cursorStyle?.classList.remove("scale-up");
 
   links.forEach((link) => {
     link.addEventListener("mouseenter", handleLinkHover);
@@ -20,21 +20,46 @@ export function enableCursorStyle() {
   };
 }
 
+
 export function trackCursorMovement(show: boolean) {
   const cursor = document.getElementById("cursor-style");
 
+  const handleMouseDown = () => cursor?.classList.add("mouse-down");
+  const handleMouseUp = () => cursor?.classList.remove("mouse-down");
+
   const moveCursor = (e: MouseEvent) => {
     if (cursor && show) {
+      cursor.classList.add("active");
       cursor.style.left = `${e.clientX}px`;
       cursor.style.top = `${e.clientY}px`;
     }
   };
 
+  const downCursor = (e: MouseEvent) => {
+    if (cursor && show) {
+      handleMouseDown();
+    }
+  };
+
+  const upCursor = (e: MouseEvent) => {
+    if (cursor && show) {
+      handleMouseUp();
+    }
+  };
+
   if (show) {
     window.addEventListener("mousemove", moveCursor);
+    window.addEventListener("mousedown", downCursor);
+    window.addEventListener("mouseup", upCursor);
   } else {
     window.removeEventListener("mousemove", moveCursor);
+    window.removeEventListener("mousedown", downCursor);
+    window.removeEventListener("mouseup", upCursor);
   }
 
-  return () => window.removeEventListener("mousemove", moveCursor);
+  return () => {
+    window.removeEventListener("mousemove", moveCursor)
+    window.removeEventListener("mousedown", downCursor);
+    window.removeEventListener("mouseup", upCursor);
+  };
 }
