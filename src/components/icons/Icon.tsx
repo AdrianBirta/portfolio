e40efac1e29586d89c6e97ba7d5cb1be844990f7
Adrian Birta <52926500+AdrianBirta@users.noreply.github.com>
2026-04-03 14:15:15 +0300

    fix(adrian/Icon.tsx): fix typescript mismatch

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -1,7 +1,14 @@
 // src\components\icons\Icon.tsx
+import { ComponentType } from "react";
 import { iconMap } from "@/components/icons/iconMap";
 
 type IconName = keyof typeof iconMap;
+
+type IconComponentProps = {
+  size?: number | string;
+  color?: string;
+  className?: string;
+};
 
 type Props = {
   name: IconName;
@@ -11,7 +18,7 @@ type Props = {
 }
 
 export default function Icon({ name, size, color, className }: Props) {
-  const Component = iconMap[name];
+  const Component = iconMap[name] as ComponentType<IconComponentProps>;
 
   if (!Component) return null;
 
